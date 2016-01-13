9ee0bb83212d39fd72225cf3d8a6d0897a30fad0
yozz <yozz.online@gmail.com>
2016-01-13 16:35:43 -0500

    Format

--- startup-cpt-home.php
+++ startup-cpt-home.php
@@ -182,7 +182,7 @@ if ( !function_exists( 'shortcode_ui_detection' ) ) {
         }
     }
 
-add_action( 'init', 'shortcode_ui_detection' );
+    add_action( 'init', 'shortcode_ui_detection' );
 }
 
 function startup_cpt_home_shortcode_ui() {
